9771b35edb82eed7f4e7e033b13adf19fc9c2afe
virsraitis <raitis.breijers@gmail.com>
2026-09-14 20:40:42 +0300

    Fix regex escapes, Ruff formatting

--- wagtail_shiki/blocks.py
+++ wagtail_shiki/blocks.py
@@ -26,8 +26,8 @@ class CodeBlock(StructBlock):
         # Languages included in PrismJS core
         # Review: https://github.com/PrismJS/prism/blob/gh-pages/prism.js#L602
         self.INCLUDED_LANGUAGES = (
-            ("ansi", "ANSI"),
-            ("text", "Plain Text"),
+            ('ansi', 'ANSI'),
+            ('text', 'Plain Text'),
         )
 
         if local_blocks is None:
@@ -40,54 +40,54 @@ class CodeBlock(StructBlock):
         local_blocks.extend(
             [
                 (
-                    "language",
+                    'language',
                     ChoiceBlock(
                         choices=language_choices,
-                        help_text=_("Coding language"),
-                        label=_("Language"),
+                        help_text=_('Coding language'),
+                        label=_('Language'),
                         default=language_default
                         or self.get_initial_languge(language_choices),
-                        identifier="language",
+                        identifier='language',
                     ),
                 ),
                 (
-                    "show_line_numbers",
+                    'show_line_numbers',
                     BooleanBlock(
-                        label=_("Show line numbers"),
+                        label=_('Show line numbers'),
                         default=True,
                         required=False,
-                        identifier="show_line_number",
-                        help_text=_("show if checked."),
+                        identifier='show_line_number',
+                        help_text=_('show if checked.'),
                     ),
                 ),
                 (
-                    "start_number",
+                    'start_number',
                     IntegerBlock(
-                        label=_("Start number"),
+                        label=_('Start number'),
                         required=False,
-                        identifier="start_number",
-                        help_text=_("(Optional)."),
+                        identifier='start_number',
+                        help_text=_('(Optional).'),
                     ),
                 ),
                 (
-                    "title",
+                    'title',
                     CharBlock(
-                        label=_("Title"),
+                        label=_('Title'),
                         required=False,
-                        identifier="title",
+                        identifier='title',
                         help_text=_(
                             "Filename, etc. Leave this blank, if you don't need."
                         ),
                     ),
                 ),
-                ("code", TextBlock(label=_("Code"), identifier="code")),
+                ('code', TextBlock(label=_('Code'), identifier='code')),
                 (
-                    "highlight_words",
+                    'highlight_words',
                     TextBlock(
-                        label=_("Highlight Words"),
+                        label=_('Highlight Words'),
                         required=False,
-                        identifier="highlight_words",
-                        help_text=_("Decoration definition data.(uneditable)"),
+                        identifier='highlight_words',
+                        help_text=_('Decoration definition data.(uneditable)'),
                     ),
                 ),
             ]
@@ -96,17 +96,17 @@ class CodeBlock(StructBlock):
         super().__init__(local_blocks, **kwargs)
 
     def get_initial_languge(self, language_choices):
-        initial_lang = getattr(settings, "WAGS_INITIAL_LANGUAGE", "")
+        initial_lang = getattr(settings, 'WAGS_INITIAL_LANGUAGE', '')
         if initial_lang in [lang[0] for lang in language_choices]:
             return initial_lang
         else:
-            return "text"
+            return 'text'
 
     def get_language_choice_list(self, **kwargs):
         # Get default languages
         specified_languages = get_language_choices()
         # If a language is passed in as part of a code block, use it.
-        language = kwargs.get("language", False)
+        language = kwargs.get('language', False)
 
         total_language_choices = specified_languages + self.INCLUDED_LANGUAGES
 
@@ -117,25 +117,25 @@ class CodeBlock(StructBlock):
                     language_default = language_choice[0]
         else:
             language_choices = specified_languages + self.INCLUDED_LANGUAGES
-            language_default = kwargs.get("default_language")
+            language_default = kwargs.get('default_language')
 
         return language_choices, language_default
 
     class Meta:
-        icon = "code"
-        template = "wagtail_shiki/code_block.html"
-        form_classname = "code-block struct-block"
-        form_template = "wagtail_shiki/code_block_form.html"
+        icon = 'code'
+        template = 'wagtail_shiki/code_block.html'
+        form_classname = 'code-block struct-block'
+        form_template = 'wagtail_shiki/code_block_form.html'
 
 
 class CodeBlockAdapter(StructBlockAdapter):
-    js_constructor = "wagtail_shiki.blocks.CodeBlock"
+    js_constructor = 'wagtail_shiki.blocks.CodeBlock'
 
     @cached_property
     def media(self):
         structblock_media = super().media
         return Media(
-            js=structblock_media._js + ["wagtail_shiki/js/wagtail-shiki-admin.js"],
+            js=structblock_media._js + ['wagtail_shiki/js/wagtail-shiki-admin.js'],
             # ["js/wagtail-shiki-admin.js"],
             css=structblock_media._css,
         )
--- wagtail_shiki/templatetags/wagtail_shiki_tags.py
+++ wagtail_shiki/templatetags/wagtail_shiki_tags.py
@@ -1,15 +1,15 @@
-from django import template
-from django.utils.safestring import mark_safe
-from django.conf import settings
-from ..settings import DEFAULT_DECORATION_OPTIONS, DEFAULT_CLASS_PREFIX
-
 import re
+
+from django import template
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+from ..settings import DEFAULT_CLASS_PREFIX, DEFAULT_DECORATION_OPTIONS
 
 register = template.Library()
 
 
 class SetVarNode(template.Node):
-
     def __init__(self, var_name, var_value):
         self.var_name = var_name
         self.var_value = var_value
@@ -18,63 +18,63 @@ class SetVarNode(template.Node):
         try:
             value = template.Variable(self.var_value).resolve(context)
         except template.VariableDoesNotExist:
-            value = ""
+            value = ''
         context[self.var_name] = value
-        return ""
+        return ''
 
 
 @register.tag(name='set')
 def set_var(parser, token):
     """
-        {% set <var_name>  = <var_value> %}
+    {% set <var_name>  = <var_value> %}
     """
     parts = token.split_contents()
     if len(parts) < 4:
         raise template.TemplateSyntaxError(
-            "'set' tag must be of the form:  {% set <var_name>  = <var_value> %}")
+            "'set' tag must be of the form:  {% set <var_name>  = <var_value> %}"
+        )
     return SetVarNode(parts[1], parts[3])
 
 
 @register.simple_tag
 def theme():
-    return getattr(settings, "WAGS_THEME", "nord")
+    return getattr(settings, 'WAGS_THEME', 'nord')
 
 
 @register.simple_tag
 def dark_theme():
-    return getattr(settings, "WAGS_DARK_THEME", theme())
+    return getattr(settings, 'WAGS_DARK_THEME', theme())
 
 
 @register.simple_tag
 def plugin_line_numbers():
-    return getattr(settings, "WAGS_LINE_NUMBERS", True)
+    return getattr(settings, 'WAGS_LINE_NUMBERS', True)
 
 
 @register.simple_tag
 def plugin_copy_to_clipboard():
-    return getattr(settings, "WAGS_COPY_TO_CLIPBOARD", True)
+    return getattr(settings, 'WAGS_COPY_TO_CLIPBOARD', True)
 
 
 @register.simple_tag
 def skip_leading_spaces():
-    return getattr(settings, "WAGS_SKIP_LEADING_SPACES", True)
+    return getattr(settings, 'WAGS_SKIP_LEADING_SPACES', True)
 
 
 @register.simple_tag
 def remove_decorations_front_spaces():
-    return getattr(settings, "WAGS_DECORATONS_REMOVE_FRONT_SPAACE", True)
+    return getattr(settings, 'WAGS_DECORATONS_REMOVE_FRONT_SPAACE', True)
 
 
 @register.simple_tag
 def remove_decorations_rear_spaces():
-    return getattr(settings, "WAGS_DECORATONS_REMOVE_REAR_SPAACE", True)
+    return getattr(settings, 'WAGS_DECORATONS_REMOVE_REAR_SPAACE', True)
 
 
 @register.simple_tag
 def decoration_options():
     select_options = []
-    options = getattr(
-        settings, "WAGS_DECORATION_OPTIONS", DEFAULT_DECORATION_OPTIONS)
+    options = getattr(settings, 'WAGS_DECORATION_OPTIONS', DEFAULT_DECORATION_OPTIONS)
 
     for opt in options:
         if opt['value'] != '':
@@ -82,10 +82,12 @@ def decoration_options():
         else:
             prefix = ''
 
-        select_options.append({
-            'value': prefix + re.sub('[\<\>\'\"\&]', '', opt['value']),
-            'text': re.sub('[\<\>\'\"\&]', '', opt['text'])
-        })
+        select_options.append(
+            {
+                'value': prefix + re.sub('[<>\'"&]', '', opt['value']),
+                'text': re.sub('[<>\'"&]', '', opt['text']),
+            }
+        )
 
     return mark_safe(str(select_options))
 
@@ -93,8 +95,7 @@ def decoration_options():
 @register.simple_tag
 def decoration_styles():
     style_rules = []
-    options = getattr(
-        settings, "WAGS_DECORATION_OPTIONS", DEFAULT_DECORATION_OPTIONS)
+    options = getattr(settings, 'WAGS_DECORATION_OPTIONS', DEFAULT_DECORATION_OPTIONS)
 
     for opt in options:
         if opt['value'] != '':
@@ -103,8 +104,8 @@ def decoration_styles():
             prefix = ''
 
         style_rule = {
-            'class': prefix + re.sub('[\<\>\'\"\&]', '', opt['value']),
-            'style': re.sub('[\<\>\'\"\&]', '', opt['style'])
+            'class': prefix + re.sub('[<>\'"&]', '', opt['value']),
+            'style': re.sub('[<>\'"&]', '', opt['style']),
         }
         style_rules.append(style_rule)
 
@@ -113,9 +114,9 @@ def decoration_styles():
 
 @register.simple_tag
 def show_highlightwords_input():
-    return getattr(settings, "WAGS_SHOW_HIGHLIGHTWORDS_INPUT", False)
+    return getattr(settings, 'WAGS_SHOW_HIGHLIGHTWORDS_INPUT', False)
 
 
 @register.simple_tag
 def class_prefix():
-    return getattr(settings, "WAGS_CLASS_PREFIX", DEFAULT_CLASS_PREFIX)
+    return getattr(settings, 'WAGS_CLASS_PREFIX', DEFAULT_CLASS_PREFIX)
